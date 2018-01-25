d2765b192af33a8649d8c8e98e997533e26395e7
gabrielwong159 <gabrielwong159@gmail.com>
2018-01-25 17:06:01 +0800

    Switched to headless mode, now shows results of hunts

--- mousehunt.py
+++ mousehunt.py
@@ -3,6 +3,7 @@ import time
 import random
 from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.remote.remote_connection import LOGGER
 from cv import read_captcha
 
 with open("facebook_credentials.json", "r") as f:
@@ -17,6 +18,7 @@ iframe_id = "iframe_canvas"
 captcha_image_class_name = "mousehuntPage-puzzle-form-captcha-image"
 captcha_input_class_name = "mousehuntPage-puzzle-form-code"
 captcha_button_class_name = "mousehuntPage-puzzle-form-code-button"
+journal_entry_id = "journallatestentry"
 
 def wait_for_next_horn():
     for i in range(15):
@@ -40,13 +42,26 @@ def check_captcha():
         driver.find_element_by_class_name(captcha_button_class_name).click()
         driver.switch_to_default_content()
 
+        print("Captcha at:", datetime.datetime.now().strftime("%d-%m-%Y %H:%M:%S"))
+
         sound_the_horn()
     except NoSuchElementException:
         print("Buwuu")
+        print(get_latest_entry())
+
+def get_latest_entry():
+    iframe = driver.find_element_by_id(iframe_id)
+    driver.switch_to_frame(iframe)
+
+    text = driver.find_element_by_id(journal_entry_id).text
+    
+    driver.switch_to_default_content()
+    return text
 
 if __name__ == "__main__":
     options = webdriver.ChromeOptions()
     options.add_argument("headless")
+    options.add_argument("log-level=2")
     driver = webdriver.Chrome(chrome_options=options)
     driver.delete_all_cookies()
 
@@ -60,7 +75,7 @@ if __name__ == "__main__":
     # await horning
     driver.get(game_url)
     print("Ready")
-    sound_the_horn()
+    #sound_the_horn()
 
     while True:
         wait_for_next_horn()
