6ff057e938295e219ed9af03c6c2314c46521995
gabrielwong159 <gabrielwong159@gmail.com>
2018-02-10 00:57:33 +0800

    Added functions for travelling and changing bait

--- driver.py
+++ driver.py
@@ -4,8 +4,10 @@ import os
 import random
 import time
 from selenium import webdriver
-from selenium.common.exceptions import NoSuchElementException
-from selenium.common.exceptions import WebDriverException
+from selenium.common.exceptions import NoSuchElementException, WebDriverException
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.remote.remote_connection import LOGGER
 from dotenv import find_dotenv, load_dotenv
 from cv import read_captcha
@@ -15,13 +17,7 @@ class MouseHuntDriver(object):
     login_url = "https://www.facebook.com/login.php"
     game_url = "https://apps.facebook.com/mousehunt/"
     horn_url = "https://apps.facebook.com/mousehunt/turn.php"
-
-    iframe_id = "iframe_canvas"
-    captcha_image_class_name = "mousehuntPage-puzzle-form-captcha-image"
-    captcha_input_class_name = "mousehuntPage-puzzle-form-code"
-    captcha_button_class_name = "mousehuntPage-puzzle-form-code-button"
-    new_code_class_name = "mousehuntPage-puzzle-form-newCode"
-    journal_entry_id = "journallatestentry"
+    travel_url = "https://apps.facebook.com/mousehunt/travel.php"
         
     def __init__(self, headless=True):
         options = webdriver.ChromeOptions()
@@ -66,18 +62,16 @@ class MouseHuntDriver(object):
         try:
             if n > 0: self.change_captcha() # if previously failed, change captcha first
             
-            iframe = driver.find_element_by_id(self.iframe_id)
-            driver.switch_to_frame(iframe)
-
-            captcha = driver.find_element_by_class_name(self.captcha_image_class_name)
+            self.switch_to_iframe()
+            captcha = driver.find_element_by_class_name("mousehuntPage-puzzle-form-captcha-image")
             image_url = captcha.value_of_css_property("background-image")[5:-2] # url("____")
             text = read_captcha(image_url)
             if (len(text) != 5):
                 raise InvalidCaptchaException()
 
-            driver.find_element_by_class_name(self.captcha_input_class_name).send_keys(text)
-            driver.find_element_by_class_name(self.captcha_button_class_name).click()
-            driver.switch_to_default_content()
+            driver.find_element_by_class_name("mousehuntPage-puzzle-form-code").send_keys(text)
+            driver.find_element_by_class_name("mousehuntPage-puzzle-form-code-button").click()
+            self.switch_to_default_content()
 
             print("Captcha at:", datetime.datetime.now().strftime("%d-%m-%Y %H:%M:%S"), "-", text)
             self.sound_the_horn()
@@ -95,13 +89,12 @@ class MouseHuntDriver(object):
         driver = self._driver
         
         driver.get(self.game_url)
-        iframe = driver.find_element_by_id(self.iframe_id)
-        driver.switch_to_frame(iframe)
+        self.switch_to_iframe()
         
-        new_captcha = driver.find_element_by_class_name(self.new_code_class_name)
+        new_captcha = driver.find_element_by_class_name("mousehuntPage-puzzle-form-newCode")
         new_captcha.find_element_by_tag_name("a").click()
 
-        driver.switch_to_default_content()
+        self.switch_to_default_content()
         driver.get(self.game_url)
 
     def get_latest_entry(self):
@@ -109,18 +102,70 @@ class MouseHuntDriver(object):
         
         # switch to iframe if available - not available when captcha
         try:
-            iframe = driver.find_element_by_id(self.iframe_id)
-            driver.switch_to_frame(iframe)
+            self.switch_to_iframe()
         except NoSuchElementException:
             pass
 
         # avoid halting the entire process when no journal entry is found
         try:
-            text = driver.find_element_by_id(self.journal_entry_id).text
-            driver.switch_to_default_content()
+            text = driver.find_element_by_id("journallatestentry").text
             return text
         except NoSuchElementException:
             return "Could not find journal entry"
+        finally:
+            self.switch_to_default_content()
+
+    def travel(self, region, location):
+        driver = self._driver
+        driver.get(self.travel_url)
+        self.switch_to_iframe()
+
+        map_regions = driver.find_elements_by_class_name("travelPage-map-region-name")
+        for element in map_regions:
+            if element.text == region: element.click()
+
+        map_locations = driver.find_elements_by_class_name("travelPage-map-region-environment-link")
+        for element in map_locations:
+            if element.text == location: element.click()
+
+        travel_buttons = driver.find_elements_by_class_name("travelPage-map-image-environment-button")
+        for element in travel_buttons:
+            if element.is_displayed(): element.click()
+
+        self.switch_to_default_content()
+
+    def is_bait_empty(self):
+        self.switch_to_iframe()
+        bait = self._driver.find_element_by_class_name("bait")
+        bait_empty = "empty" in bait.get_attribute("class").split()
+        self.switch_to_default_content()
+        return bait_empty
+
+    def change_bait(self, bait_name):
+        driver = self._driver
+        self.switch_to_iframe()
+
+        trap_controls = driver.find_elements_by_class_name("trapControlThumb")
+        for element in trap_controls:
+            if element.get_attribute("data-classification") == "bait": element.click()
+
+        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, "trapComponentRow")))
+        baits = driver.find_elements_by_class_name("trapComponentRow")
+        for element in baits:
+            if "selected" in element.get_attribute("class"): continue
+            cheese = element.find_element_by_class_name("name").text
+            if cheese == bait_name:
+                element.click()
+                break
+
+        self.switch_to_default_content()
+
+    def switch_to_iframe(self):
+        iframe = self._driver.find_element_by_id("iframe_canvas")
+        self._driver.switch_to_frame(iframe)
+
+    def switch_to_default_content(self):
+        self._driver.switch_to_default_content()
 
     def close(self):
         self._driver.close()
--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from driver import MouseHuntDriver
 def main():
     # automatically resets when an unknown error is encountered
     try:
-        driver = MouseHuntDriver(headless=True)
+        driver = MouseHuntDriver(headless=False)
         driver.login()
         while True:
             driver.sound_the_horn()
