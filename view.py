1d1d237ed835baa744049235bf649496b915bf4a
Gabriel Wong <gabrielwong159@gmail.com>
2019-04-10 18:21:36 +0800

    Update trap setup code for new UI

--- view.py
+++ view.py
@@ -1,5 +1,9 @@
 from util.driver import MouseHuntDriver
+from util.config import get_login_config
+
 
 if __name__ == "__main__":
+    username, password = get_login_config()
+    
     driver = MouseHuntDriver(headless=False)
-    driver.login()
+    driver.login(username, password)
